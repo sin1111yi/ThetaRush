13742b9e396e52797bad5be768b8605b1bddf509
sin1111yi <sin1111yi@foxmail.com>
2025-05-18 22:48:17 +0800

    update trll

--- src/main/tr/ds/ll.h
+++ src/main/tr/ds/ll.h
@@ -28,8 +28,8 @@
  */
 typedef enum
 {
-  TR_LL_FORWARD, /**< Traverse from head to tail */
-  TR_LL_BACKWARD /**< Traverse from tail to head */
+  TR_LL_DIRECTION_FORWARD, /**< Traverse from head to tail */
+  TR_LL_DIRECTION_BACKWARD /**< Traverse from tail to head */
 } trLLDirection_t;
 
 /* Node structure for doubly linked list */
@@ -46,17 +46,17 @@ typedef struct __trLLNode_s
 typedef trLLNode_t trLL_t;
 
 #if !defined(TR_LL_HEAP_SIZE)
-#define TR_LL_HEAP_SIZE 128
-#define __TR_LL_GUARD_SIZE (TR_LL_HEAP_SIZE / 32)
+#define TR_LL_HEAP_SIZE (128)
+#define __TR_LL_GUARD_SIZE ((TR_LL_HEAP_SIZE + 31) / 32)
 #endif
 
 /* Memory heap management structure */
 typedef struct __trLLMemHeap_s
 {
-  trLLNode_t memory[TR_LL_HEAP_SIZE]; /**< Preallocated node array */
-  uint32_t guard[__TR_LL_GUARD_SIZE]; /**< Bitmap tracking usage */
-  trLLNode_t *(*new) (void);          /**< Allocation function */
-  void (*free) (trLLNode_t *);        /**< Deallocation function */
+  trLLNode_t memPool[TR_LL_HEAP_SIZE]; /**< Preallocated node array */
+  uint32_t guard[__TR_LL_GUARD_SIZE];  /**< Bitmap tracking usage */
+  trLLNode_t *(*new) (void);           /**< Allocation function */
+  void (*free) (trLLNode_t *);         /**< Deallocation function */
 } trLLMemHeap_t;
 
 /* Modular interface structure */
@@ -64,56 +64,55 @@ typedef struct __trLLModule_s
 {
   trLLMemHeap_t *heap; /**< Associated memory heap */
 
-  trLL_t *(*create) (void);                     /**< Create new list */
-  int (*pushBack) (trLL_t *, uint32_t, void *); /**< Add node to list tail */
-  bool (*popNode) (trLL_t *, uint32_t);         /**< Remove node by ID */
-  trLLNode_t *(*find) (trLL_t *, uint32_t);     /**< Find node by ID */
-  uint32_t (*getLength) (trLL_t *);             /**< Get linear list length */
+  trLL_t *(*create) (void);                       /**< Create new list */
+  void (*destroy) (trLL_t **);                    /**< Destroy entire list */
+  int (*pushBack) (trLL_t *, void *,  uint32_t *); /**< Add node to list tail */
+  trLLNode_t *(*find) (trLL_t *, uint32_t);       /**< Find node by ID */
+  int (*popNode) (trLL_t *, uint32_t);            /**< Remove node by ID */
+  uint32_t (*getLength) (trLL_t *); /**< Get linear list length */
   uint32_t (*getLengthEx) (
       trLL_t *, trLLDirection_t); /**< Get linear list length with direction */
   uint32_t (*traverse) (trLL_t *, void (*) (trLLNode_t *), trLLDirection_t);
-  void (*destroy) (trLL_t **); /**< Destroy entire list */
 
-  trLL_t *(*cllCreate) (uint8_t); /**< Create circular list */
+  trLL_t *(*cllCreate) (void);    /**< Create circular list */
   bool (*cllIf) (trLL_t *);       /**< Check circular linkage */
-  int (*cllPushBack) (trLL_t *, uint32_t, void *);
-  bool (*cllPopNode) (trLL_t *, uint32_t);
+  void (*cllDestroy) (trLL_t **); /**< Circular list destruction */
+  int (*cllPushBack) (trLL_t *, void *, uint32_t *);
   trLLNode_t *(*cllFindNode) (trLL_t *, uint32_t);
+  int (*cllPopNode) (trLL_t *, uint32_t);
   uint32_t (*cllGetLength) (trLL_t *); /**< Get circular list length */
   uint32_t (*cllGetLengthEx) (
       trLL_t *,
       trLLDirection_t); /**< Get circular list length with direction */
   uint32_t (*cllTraverse) (trLL_t *, void (*) (trLLNode_t *),
                            trLLDirection_t); /**< Safe traversal */
-  bool (*cllBreak) (trLL_t *);               /**< Convert to linear */
-  void (*cllRotate) (trLL_t *, int32_t);     /**< Rotate circular list */
-  void (*cllDestroy) (trLL_t **);            /**< Circular list destruction */
+  int (*cllBreak) (trLL_t *);                /**< Convert to linear */
 } trLLInterface_t;
 
 trLLNode_t *trLLHeapAllocNode (void);
 void trLLHeapFreeNode (trLLNode_t *node);
 
 trLL_t *trLLCreate (void);
-int trLLPushNode (trLL_t *head, uint32_t id, void *data);
-bool trLLPopNode (trLL_t *head, uint32_t id);
+void trLLDestroy (trLL_t **head);
+int trLLPushBack (trLL_t *head, void *data, uint32_t *id);
 trLLNode_t *trLLFindNode (trLL_t *head, uint32_t id);
+int trLLPopNode (trLL_t *head, uint32_t id);
 uint32_t trLLGetLength (trLL_t *head);
 uint32_t trLLGetLengthEx (trLL_t *head, trLLDirection_t dir);
 uint32_t trLLTraverse (trLL_t *head, void (*visit) (trLLNode_t *),
                        trLLDirection_t dir);
-void trLLDestroy (trLL_t **head);
 
-trLL_t *trCLLCreate (uint8_t count);
+trLL_t *trCLLCreate (void);
 bool trCLLIf (trLL_t *head);
-int trCLLPushBack (trLL_t *head, uint32_t id, void *data);
-bool trCLLPopNode (trLL_t *head, uint32_t id);
+void trCLLDestroy (trLL_t **head);
+int trCLLPushBack (trLL_t *head, void *data, uint32_t *id);
 trLLNode_t *trCLLFindNode (trLL_t *head, uint32_t id);
+int trCLLPopNode (trLL_t *head, uint32_t id);
 uint32_t trCLLGetLength (trLL_t *head);
 uint32_t trCLLGetLengthEx (trLL_t *head, trLLDirection_t dir);
 uint32_t trCLLTraverse (trLL_t *head, void (*visit) (trLLNode_t *),
                         trLLDirection_t dir);
-bool trLCLLBreak (trLL_t *head);
-void trLCLLRotate (trLL_t *head, int32_t steps);
-void trCLLDestroy (trLL_t **head);
+int trLCLLBreak (trLL_t *head);
 
-trLLInterface_t* trLLInterfaceHandle(void);
+void trLLInterfaceInit (void);
+trLLInterface_t *trLLInterfaceHandle (void);
